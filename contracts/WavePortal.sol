b44c0ec3a0ecbb83391bca9e33cc169233dcb855
Massimo Di Berardino <massimokris22@gmail.com>
2021-10-10 22:59:14 -0300

    WavePortal structure done, deploy script for local network, scripts added to package.json

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo yo, I'm a contract and I'm smart");
+    }
+
+    function wave() public {
+        totalWaves++;
+        // msh.sender is the wallet address who called the function
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
     }
 }
